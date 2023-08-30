3180345584e1e620d112d440f61ebced7629f0e0
rchenchaiah98 <rchenchaiah98@gmail.com>
2023-08-30 13:06:09 +0000

    buttons

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -3,7 +3,21 @@ import {Component} from 'react'
 import './index.css'
 
 class DigitalTimer extends Component {
-  state = {time: 25}
+  state = {time: 25, isTimerRunning: false, timer: 0}
+
+  componentDidMount() {
+    this.timerID = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick = () => {
+    this.setState(prevState => ({
+      timer: prevState.timer + 1,
+    }))
+  }
 
   onIncrement = () => {
     this.setState(prevState => ({time: prevState.time + 1}))
@@ -13,21 +27,34 @@ class DigitalTimer extends Component {
     this.setState(prevState => ({time: prevState.time - 1}))
   }
 
+  startPause = () => {
+    const {isTimerRunning} = this.state
+    this.setState(prevState => ({isTimerRunning: !prevState.isTimerRunning}))
+    if (isTimerRunning) {
+      clearInterval(this.timerID)
+    }
+  }
+
   render() {
-    const {time} = this.state
+    const {time, isTimerRunning, timer} = this.state
+
     return (
       <div className="App-container">
         <h1>Digital Timer</h1>
         <div className="main-container">
           <div className="timer-container">
             <div className="display-time">
-              <p>{time}</p>
+              <p className="timer-text">{timer}</p>
             </div>
           </div>
           <div className="buttons-container">
             <div className="start-pause-button-container">
               <div>
-                <button type="button" className="button">
+                <button
+                  type="button"
+                  className="button button-text"
+                  onClick={this.startPause}
+                >
                   <div className="img-container">
                     <img
                       className="button-img"
@@ -35,7 +62,9 @@ class DigitalTimer extends Component {
                       alt="play"
                     />
                   </div>
-                  <div className="button-text">start</div>
+                  <div type="button" className="">
+                    {isTimerRunning ? 'pause' : 'start'}
+                  </div>
                 </button>
               </div>
               <div>
@@ -64,7 +93,7 @@ class DigitalTimer extends Component {
                   </button>
                 </div>
                 <div className="user-enter-time">
-                  <p>{time}</p>
+                  <p className="timer-text">{time}</p>
                 </div>
                 <div>
                   <button
